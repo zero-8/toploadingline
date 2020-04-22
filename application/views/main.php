662b7c39b29623a1612097d301325d847c72ec7f
zero-8 <jmangaluz@gmail.com>
2020-04-22 15:30:48 +0800

    add loader

---
+++ application/views/main.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-a-compatible" content="ie-edge">
+	<title>Top loaded effects</title>
+
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+ 
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script  src="https://code.jquery.com/jquery-3.5.0.js"  integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="yt-loader"></div>
+
+</body>
+</html>
